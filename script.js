a29167841ae52ffb3d52240becb4fbe886f20d74
Shayan <shayan.mahnam.uk@gmail.com>
2023-01-31 20:48:17 +0000

    Revert "bug fixed"
    This reverts commit 4648859d4ec6916ce3c430ea890f4cb7dd9b61d7.

--- script.js
+++ script.js
@@ -22,15 +22,61 @@ searchInput.type = "text";
 searchInput.placeholder = "Search";
 
 function displayEpisodesWithSearch(episodes) {
-  // Display dropbox
-  displayDropbox(episodes)
+  const list = document.createElement("ul");
+  let count = 0;
+  const searchInput = document.createElement("input");
+  searchInput.type = "text";
+  searchInput.placeholder = "Search";
+
+  const select = document.createElement("select");
+  const allOption = document.createElement("option");
+  allOption.value = "all";
+  allOption.innerHTML = "All episodes";
+  select.appendChild(allOption);
+
+  episodes.forEach(function (episode) {
+    const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+    const option = document.createElement("option");
+    option.value = episode.id;
+    option.innerHTML = `${episodeCode} - ${episode.name}`;
+    select.appendChild(option);
+  });
+
+  select.addEventListener("change", function () {
+    let filteredEpisodes = [];
+    if (select.value === "all") {
+      filteredEpisodes = episodes;
+    } else {
+      filteredEpisodes = episodes.filter(ep => select.value.includes(ep.id) );
+    }
+    count = 0;
+    list.innerHTML = "";
+    filteredEpisodes.forEach(function (episode) {
+      const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+        const item = document.createElement("li");
+        const title =  document.createElement("h3")
+        const image = document.createElement("img");
+        const sum = document.createElement("p")
+        image.src = episode.image.medium;
+        title.innerText = `${episode.name} - ${episodeCode}`;
+        sum.innerHTML = episode.summary; 
+
+        item.appendChild(title)
+        item.appendChild(image);
+        item.appendChild(sum)
+        list.appendChild(item);
+        count++;
+    });
+    countSpan.innerHTML = `${count} episode(s) found out of 73`;
+  });
+
 
   // Display all episodes initially
   displayEpisodes(episodes)
   
   const countSpan = document.createElement("span");
-  countSpan.innerHTML = `${count} episode(s) found out of ${episodes.length}`;
-  appHeader.appendChild(countSpan);
+  countSpan.innerHTML = `${count} episode(s) found out of 73`;
+  main.appendChild(countSpan);
   
   searchInput.addEventListener("input", function () {
     const searchTerm = searchInput.value.toLowerCase();
@@ -57,7 +103,7 @@ function displayEpisodesWithSearch(episodes) {
         count++;
       }
     });
-    countSpan.innerHTML = `${count} episode(s) found out of ${episodes.length}`;
+    countSpan.innerHTML = `${count} episode(s) found out of 73`;
   });
   appHeader.appendChild(select);
   appHeader.appendChild(searchInput);
