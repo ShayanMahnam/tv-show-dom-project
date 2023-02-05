04f0589dbaba4876c01bc9c29cf56d50c796b8b0
Shayan <shayan.mahnam.uk@gmail.com>
2023-02-05 23:09:17 +0000

    refactor until level 300

--- script.js
+++ script.js
@@ -1,129 +1,83 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  
-  displayEpisodesWithSearch(allEpisodes)
+  createOptions(allEpisodes)
+  makePageForEpisodes(allEpisodes);
 }
 
-
-window.onload = setup;
-
-//assigns variables
-const main = document.getElementById("main")
-const appHeader = document.getElementById("head-app")
-const list = document.createElement("ul");
-let count = 0;
-
-// hole function need to refactor it
-
-function displayEpisodesWithSearch(episodes) {
-  // initial assigning variables
-  let count = 0;
-  const searchInput = document.createElement("input");
-  searchInput.type = "text";
-  searchInput.placeholder = "Search";
-
-  const select = document.createElement("select");
-  const allOption = document.createElement("option");
-
-  // first option for all episodes
-  allOption.value = "all";
-  allOption.innerHTML = "All episodes";
-  select.appendChild(allOption);
-
-  // display the rest of select options
+function createOptions(episodes) {
   episodes.forEach(function (episode) {
-    const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
     const option = document.createElement("option");
     option.value = episode.id;
-    option.innerHTML = `${episodeCode} - ${episode.name}`;
-    select.appendChild(option);
+    option.innerHTML = `${makeSeasonAndEpisode(episode)} - ${episode.name}`
+    dropEpisodes.appendChild(option);
   });
-
-  // dropBox event
-  select.addEventListener("change", function () {
-    let filteredEpisodes = [];
-    if (select.value === "all") {
-      filteredEpisodes = episodes;
-    } else {
-      filteredEpisodes = episodes.filter(ep => select.value.includes(ep.id) );
-    }
-    count = 0;
-    list.innerHTML = "";
-    filteredEpisodes.forEach(function (episode) {
-      const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
-        const item = document.createElement("li");
-        const title =  document.createElement("h3")
-        const image = document.createElement("img");
-        const sum = document.createElement("p")
-        image.src = episode.image.medium;
-        title.innerText = `${episode.name} - ${episodeCode}`;
-        sum.innerHTML = episode.summary; 
-
-        item.appendChild(title)
-        item.appendChild(image);
-        item.appendChild(sum)
-        list.appendChild(item);
-        count++;
-    });
-    countSpan.innerHTML = `${count} episode(s) found out of ${episodes.length}`;
-  });
-
-
-  // Display all episodes initially
-  episodes.forEach(function (episode) {
-    const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
-        const item = document.createElement("li");
-        const title =  document.createElement("h3")
-        const image = document.createElement("img");
-        const sum = document.createElement("p")
-        image.src = episode.image.medium;
-        title.innerText = `${episode.name} - ${episodeCode}`;
-        sum.innerHTML = episode.summary; 
-
-        item.appendChild(title)
-        item.appendChild(image);
-        item.appendChild(sum)
-        list.appendChild(item);
-        count++;
-  });
-
-  // display counting
-  const countSpan = document.createElement("span");
-  countSpan.innerHTML = `${count} episode(s) found out of ${episodes.length}`;
-  main.appendChild(countSpan);
-  
-
-  // searching function
-  searchInput.addEventListener("input", function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    list.innerHTML = "";
-    count = 0;
-    episodes.forEach(function (episode) {
-      if (
-        episode.name.toLowerCase().includes(searchTerm) ||
-        episode.summary.toLowerCase().includes(searchTerm)
-      ) {
-        const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
-        const item = document.createElement("li");
-        const title =  document.createElement("h3")
-        const image = document.createElement("img");
-        const sum = document.createElement("p")
-        image.src = episode.image.medium;
-        title.innerText = `${episode.name} - ${episodeCode}`;
-        sum.innerHTML = episode.summary; 
-
-        item.appendChild(title)
-        item.appendChild(image);
-        item.appendChild(sum)
-        list.appendChild(item);
-        count++;
-      }
-    });
-    countSpan.innerHTML = `${count} episode(s) found out of ${episodes.length}`;
-  });
-  appHeader.appendChild(select);
-  appHeader.appendChild(searchInput);
-  appHeader.appendChild(countSpan);
-  main.appendChild(list);
 }
+
+function makeSeasonAndEpisode(episode) {
+  const { season, number } = episode;
+  const paddedSeason = season.toString().padStart(2, "0");
+  const paddedEpisode = number.toString().padStart(2, "0");
+
+  return `S${paddedSeason}E${paddedEpisode}`;
+}
+
+function countEpisodes(episodes){
+  const countParagraph = document.getElementById("count-episodes")
+  countParagraph.innerText = `Displaying episode(s) ${episodes.length}/${getAllEpisodes().length}`;
+}
+
+const rootElem = document.getElementById("root");
+function makePageForEpisodes(episodeList) {
+  
+  // clear out the rootElement's HTML before we add the new stuff
+  rootElem.innerHTML = "";
+  
+  countEpisodes(episodeList)
+
+  episodeList.forEach((episode) => {
+    // add list
+    const item = document.createElement("li")
+
+    // add the season and episode and name
+    const paragraph = document.createElement("h3");
+    paragraph.textContent = `${episode.name} - ${makeSeasonAndEpisode(episode)}`;
+    item.appendChild(paragraph);
+
+    // add the image
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    item.appendChild(image);
+
+    // add the summary paragraph nb the episode.summary is actually HTML
+    item.innerHTML += episode.summary;
+    rootElem.appendChild(item)
+  });
+}
+
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", (e) => {
+  const searchTerms = e.target.value.toLowerCase();
+  const filteredEpisodes = getAllEpisodes().filter((episode) => {
+    // localeCompare might be neater here
+    return (
+      episode.summary.toLowerCase().includes(searchTerms) ||
+      episode.name.toLowerCase().includes(searchTerms)
+    );
+  });
+  makePageForEpisodes(filteredEpisodes);
+});
+
+const dropEpisodes = document.getElementById("drop-episodes")
+dropEpisodes.addEventListener("change",(e) =>{
+  const select = e.target.value;
+  const filteredEpisodes = getAllEpisodes().filter((episode) => {
+    // localeCompare might be neater here
+    return (
+      select.includes(episode.id) 
+    );
+  });
+   select === "all" ? makePageForEpisodes(getAllEpisodes()) : makePageForEpisodes(filteredEpisodes)
+})
+
+window.onload = setup;
