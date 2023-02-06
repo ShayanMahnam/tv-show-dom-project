e2fad9388c66272cff8c7cfde5b20424beee838a
Shayan <shayan.mahnam.uk@gmail.com>
2023-02-06 22:38:16 +0000

    added shows and fetch

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  createOptions(allEpisodes)
-  makePageForEpisodes(allEpisodes);
+async function fetchAllShows() {
+  try {
+  const response = await fetch (`https://api.tvmaze.com/shows`);
+  const data = await response.json();
+  console.log(data);
+  return data;
+        
+    } catch (error) {
+        return error
+    }
+}
+
+async function fetchAllEpisodes(showId) {
+   
+    const response = await fetch (`https://api.tvmaze.com/shows/${showId}/episodes`);
+    const data = await response.json();
+    console.log("fetchAllEpisodes function ran:");
+    console.log(data);
+    return data;
+}
+
+
+async function setup() {
+  const allShows= await fetchAllShows();
+  createOptionsShows(allShows)
+  countShows(allShows)
+  makePageForShows(allShows)
+}
+
+function createOptionsShows(shows){
+  shows.forEach(function (show) {
+    const option = document.createElement("option");
+    option.value = show.id;
+    option.innerHTML = `${show.name}`
+    dropShows.appendChild(option);
+})
 }
 
 function createOptions(episodes) {
@@ -22,9 +53,45 @@ function makeSeasonAndEpisode(episode) {
   return `S${paddedSeason}E${paddedEpisode}`;
 }
 
-function countEpisodes(episodes){
+async function countShows(shows){
   const countParagraph = document.getElementById("count-episodes")
-  countParagraph.innerText = `Displaying episode(s) ${episodes.length}/${getAllEpisodes().length}`;
+  countParagraph.innerText = `Displaying show(s) ${shows.length}/${shows.length}`;
+}
+
+async function countEpisodes(episodes){
+  const select = document.getElementById("drop-shows").value
+  const episodesSelectedShow = await fetchAllEpisodes(select);
+
+  const countParagraph = document.getElementById("count-episodes")
+  countParagraph.innerText = `Displaying episode(s) ${episodes.length}/${episodesSelectedShow.length}`;
+}
+
+function makePageForShows(showsList){
+  rootElem.innerHTML = "";
+
+  countShows(showsList)
+
+  showsList.forEach((show) => {
+    // add list
+    const item = document.createElement("li")
+
+    // add the season and episode and name
+    const paragraph = document.createElement("h3");
+    paragraph.textContent = `${show.name}`;
+    item.appendChild(paragraph);
+
+    // add the image
+    const image = document.createElement("img");
+    image.src = show.image.medium;
+    item.appendChild(image);
+
+    // add the summary paragraph nb the episode.summary is actually HTML
+    const summary = document.createElement('p')
+    item.appendChild(summary)
+    summary.innerHTML += show.summary;
+    rootElem.appendChild(item)
+
+})
 }
 
 const rootElem = document.getElementById("root");
@@ -56,9 +123,12 @@ function makePageForEpisodes(episodeList) {
 }
 
 const searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", (e) => {
+searchInput.addEventListener("input", async (e) => {
+  const select = document.getElementById("drop-shows").value
+  const episodes = await fetchAllEpisodes(select);
+
   const searchTerms = e.target.value.toLowerCase();
-  const filteredEpisodes = getAllEpisodes().filter((episode) => {
+  const filteredEpisodes = episodes.filter((episode) => {
     // localeCompare might be neater here
     return (
       episode.summary.toLowerCase().includes(searchTerms) ||
@@ -69,15 +139,35 @@ searchInput.addEventListener("input", (e) => {
 });
 
 const dropEpisodes = document.getElementById("drop-episodes")
-dropEpisodes.addEventListener("change",(e) =>{
+dropEpisodes.addEventListener("change",async (e) =>{
+  const selectShow = document.getElementById("drop-shows").value
+  const episodes = await fetchAllEpisodes(selectShow);
   const select = e.target.value;
-  const filteredEpisodes = getAllEpisodes().filter((episode) => {
+  const filteredEpisodes = episodes.filter((episode) => {
     // localeCompare might be neater here
     return (
       select.includes(episode.id) 
     );
   });
-   select === "all" ? makePageForEpisodes(getAllEpisodes()) : makePageForEpisodes(filteredEpisodes)
+   select === "all" ? makePageForEpisodes(episodes) : makePageForEpisodes(filteredEpisodes)
+})
+
+const dropShows = document.getElementById("drop-shows")
+dropShows.addEventListener('change',async (e) => {
+ 
+  const select = e.target.value;
+  const shows = await fetchAllShows()
+  const episodes = await fetchAllEpisodes(select);
+  
+  
+    if(select === 'all-shows'){
+      rootElem.innerHTML = "";
+      makePageForShows(shows) 
+    }
+    else{
+      createOptions(episodes)
+      makePageForEpisodes(episodes)
+    }
 })
 
 window.onload = setup;
