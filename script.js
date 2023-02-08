872fe194cf5052b0be2153cdc306d0a2f8a6fdcc
Shayan <shayan.mahnam.uk@gmail.com>
2023-02-08 17:42:56 +0000

    new display added drop down works

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ async function fetchAllShows() {
     }
 }
 
+async function fetchShow(showId){
+  const response = await fetch (`https://api.tvmaze.com/shows/${showId}`);
+    const data = await response.json();
+    return data;
+}
+
 async function fetchAllEpisodes(showId) {
    
     const response = await fetch (`https://api.tvmaze.com/shows/${showId}/episodes`);
@@ -20,7 +26,20 @@ async function fetchAllEpisodes(showId) {
 
 async function setup() {
   const allShows= await fetchAllShows();
-  makePageForShows(allShows)
+  showsDisplay(allShows)
+}
+
+function createOptionsNewDisplay(shows){
+  shows.sort((a,b)=> {
+   if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  }).forEach(function (show) {
+    const option = document.createElement("option");
+    option.value = show.id;
+    option.innerHTML = `${show.name}`
+    dropNewDisplay.appendChild(option);
+})
 }
 
 function createOptionsShows(shows){
@@ -58,6 +77,11 @@ function makeSeasonAndEpisode(episode) {
   const paddedEpisode = number.toString().padStart(2, "0");
 
   return `S${paddedSeason}E${paddedEpisode}`;
+}
+
+function foundShows(shows){
+  const countParagraph = document.getElementById("count-shows")
+  countParagraph.innerText = `Found show(s) ${shows.length}`;
 }
 
 async function countShows(shows){
@@ -108,6 +132,8 @@ function makePageForEpisodes(episodeList) {
   
   // clear out the rootElement's HTML before we add the new stuff
   rootElem.innerHTML = "";
+  appHeaderNewDisplay.style.display = "none"
+  appHeader.style.display = "flex"
   
   countEpisodes(episodeList)
 
@@ -210,5 +236,171 @@ dropShows.addEventListener('change',async (e) => {
       makePageForEpisodes(episodes)
     }
 })
+
+// level 500
+// assign variables
+const appHeader = document.getElementById("head-app")
+const appHeaderNewDisplay = document.getElementById("head-app-shows")
+const dropNewDisplay = document.getElementById("shows-search-drop")
+const searchNewDisplay = document.getElementById("search-input-shows")
+
+function showsDisplay(showsList){
+  rootElem.innerHTML = "";
+  appHeader.style.display = "none"
+  appHeaderNewDisplay.style.display = "flex"
+
+  createOptionsNewDisplay(showsList)
+  foundShows(showsList)
+  
+
+  showsList.forEach((show) => {
+    // add list
+    const id = show.id
+    const item = document.createElement("li")
+    item.classList.add("li-shows")
+
+    item.addEventListener("click", async () =>{
+      const episodes = await fetchAllEpisodes(id)
+      makePageForEpisodes(episodes)
+      createOptions(episodes)
+      createOptionsShows(showsList)
+      dropShows.value = `${id}`
+    })
+
+    // add the season and episode and name
+    const paragraph = document.createElement("h3");
+    paragraph.textContent = `${show.name}`;
+    item.appendChild(paragraph);
+
+    const divDetails = document.createElement("div")
+    divDetails.classList.add("picture-details")
+    item.appendChild(divDetails)
+    // add the image
+    const image = document.createElement("img");
+    image.classList.add("image-shows")
+    image.src = show.image.medium;
+    divDetails.appendChild(image);
+
+    // add the summary paragraph nb the episode.summary is actually HTML
+    const summary = document.createElement('p')
+    summary.classList.add("summary")
+    divDetails.appendChild(summary)
+    summary.innerHTML += show.summary;
+
+    const divElement = document.createElement('div')
+    divElement.classList.add('details')
+    divDetails.appendChild(divElement)
+
+    const rated = document.createElement('h5')
+    rated.innerHTML= `rated: ${show.rating.average}`
+    divElement.appendChild(rated)
+
+    const genres = document.createElement('h5')
+    genres.innerHTML = `Genres: ${show.genres}`
+    divElement.appendChild(genres)
+
+    const status = document.createElement('h5')
+    status.innerHTML = `Status: ${show.status}`
+    divElement.appendChild(status)
+
+    const runtime = document.createElement('h5')
+    runtime.innerHTML = `Runtime: ${show.runtime}`
+    divElement.appendChild(runtime)
+
+    rootElem.appendChild(item)
+
+})
+}
+
+function showDisplay(show){
+  rootElem.innerHTML = "";
+  appHeader.style.display = "none"
+  appHeaderNewDisplay.style.display = "flex"
+
+  foundShows([0])
+
+  const id = show.id
+    const item = document.createElement("li")
+    item.classList.add("li-shows")
+
+    item.addEventListener("click", async () =>{
+      const episodes = await fetchAllEpisodes(id)
+      makePageForEpisodes(episodes)
+      createOptions(episodes)
+      createOptionsShows(showsList)
+      dropShows.value = `${id}`
+    })
+
+    // add the season and episode and name
+    const paragraph = document.createElement("h3");
+    paragraph.textContent = `${show.name}`;
+    item.appendChild(paragraph);
+
+    const divDetails = document.createElement("div")
+    divDetails.classList.add("picture-details")
+    item.appendChild(divDetails)
+    // add the image
+    const image = document.createElement("img");
+    image.classList.add("image-shows")
+    image.src = show.image.medium;
+    divDetails.appendChild(image);
+
+    // add the summary paragraph nb the episode.summary is actually HTML
+    const summary = document.createElement('p')
+    summary.classList.add("summary")
+    divDetails.appendChild(summary)
+    summary.innerHTML += show.summary;
+
+    const divElement = document.createElement('div')
+    divElement.classList.add('details')
+    divDetails.appendChild(divElement)
+
+    const rated = document.createElement('h5')
+    rated.innerHTML= `rated: ${show.rating.average}`
+    divElement.appendChild(rated)
+
+    const genres = document.createElement('h5')
+    genres.innerHTML = `Genres: ${show.genres}`
+    divElement.appendChild(genres)
+
+    const status = document.createElement('h5')
+    status.innerHTML = `Status: ${show.status}`
+    divElement.appendChild(status)
+
+    const runtime = document.createElement('h5')
+    runtime.innerHTML = `Runtime: ${show.runtime}`
+    divElement.appendChild(runtime)
+
+    rootElem.appendChild(item)
+
+
+}
+
+const button = document.getElementById("go-back")
+button.addEventListener("click",async ()=>{
+  const shows = await fetchAllShows()
+  dropNewDisplay.value = "all-search-shows"
+  searchNewDisplay.value = ''
+  showsDisplay(shows)
+})
+
+dropNewDisplay.addEventListener('change',async (e) => {
+ 
+  const select = e.target.value;
+  
+    if(select === 'all-search-shows'){
+      const shows = await fetchAllShows()
+      rootElem.innerHTML = "";
+      searchInput.value = '';
+      createOptionsNewDisplay(shows)
+      showsDisplay(shows) 
+    }
+    else{
+      const show = await fetchShow(select);
+      searchInput.value = ''
+      showDisplay(show)
+    }
+})
+
 
 window.onload = setup;
